4dac06e686dbbcefd37f505fbb0f00ea534bd22e
rodionov80 <rodionov134@gmail.com>
2021-01-19 07:04:37 +0300

    Add some material components

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,12 @@
 import '../styles/style.scss'
-import delay from './includes/delay.ts'
+// import delay from './includes/delay.ts'
+//
+// (async () => {
+//     await delay(3000)
+//     console.log('Hello world')
+// })()
 
-(async () => {
-    await delay(3000)
-    console.log('Hello world')
-})()
+import '@material/mwc-top-app-bar'
+import '@material/mwc-icon-button'
+import '@material/mwc-dialog'
+import '@material/mwc-button'
