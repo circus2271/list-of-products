7aa307164669e088070aaf8de7de32fdde9dea38
rodionov80 <rodionov134@gmail.com>
2021-01-20 06:11:40 +0300

    Add a couple of components; add lists; create open-dialog script

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -6,7 +6,15 @@ import '../styles/style.scss'
 //     console.log('Hello world')
 // })()
 
+// import '@material/mwc-icon/mwc-icon-font'
 import '@material/mwc-top-app-bar'
 import '@material/mwc-icon-button'
 import '@material/mwc-dialog'
 import '@material/mwc-button'
+import '@material/mwc-list/mwc-check-list-item.js';
+import '@material/mwc-list/mwc-list.js';
+import {Dialog} from "@material/mwc-dialog/mwc-dialog";
+
+const navbarInfoButton: HTMLButtonElement = document.querySelector('mwc-icon-button#info')
+const dialog: Dialog = document.querySelector('mwc-dialog#dialog')
+navbarInfoButton.onclick = () => dialog.show()
