1a9366ad19ff4b3e4f3ca0754b3cd5f0227e5954
rodionov80 <rodionov134@gmail.com>
2021-01-31 11:46:10 +0300

    Allow server to be accessible from local network

--- webpack/webpack.development.js
+++ webpack/webpack.development.js
@@ -7,7 +7,8 @@ module.exports = merge(common, {
   devtool: 'eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, '../dist'),
-    port: 9000
+    port: 9000,
+    host: '0.0.0.0'
   },
   module: {
     rules: [
